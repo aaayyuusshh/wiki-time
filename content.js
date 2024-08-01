f9f3b3376943c6907ea7f1fbfb800a639cef844a
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2024-07-31 23:27:07 -0600

    Disregard references when parsing article body

--- content.js
+++ content.js
@@ -5,6 +5,13 @@ function parseText(node) {
     } else if(node.nodeType === Node.ELEMENT_NODE) {
         for(let childNode of node.childNodes) {
             let childNodeHtmlTag = childNode.nodeName.toLowerCase();
+            if(childNode.nodeType === Node.ELEMENT_NODE 
+                && childNode.className.includes("mw-heading") 
+                && childNode.firstChild.nodeType == Node.ELEMENT_NODE 
+                && childNode.firstChild.getAttribute("id") === "References") {
+                    console.log("--reached references--");
+                    break;
+            }
             if(childNodeHtmlTag != "style" && childNodeHtmlTag != "script" && childNodeHtmlTag != "img") {
                 text += parseText(childNode);
             }
