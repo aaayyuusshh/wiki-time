c61d8e438895a8a97236b1fb9e743d06b8af9f44
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2024-08-27 14:44:41 -0600

    Reload open wiki tabs on wpm value change

--- background.js
+++ background.js
@@ -10,3 +10,21 @@ function openTab(path) {
         url: path
     })
 }
+
+// rerun content script when wpm value is changed through action popup
+chrome.storage.onChanged.addListener(async (changes, areaName) => {
+    if(areaName == "local" && changes.wpm) {
+        // console.log(`old wpm: ${changes.wpm?.oldValue}, new wpm: ${changes.wpm?.newValue}`);
+        let wikiTabs = await chrome.tabs.query({url: "https://en.wikipedia.org/wiki/*"});
+        console.log(wikiTabs);
+        wikiTabs.forEach((tab) => {
+            // rerunning in the same context is causing variable scoping issues
+            // just reload tabs for now as that will rerun the content script in a new context
+            // chrome.scripting.executeScript({
+            //     target: {tabId: tab.id},
+            //     files: ["content.js"]
+            // });
+            chrome.tabs.reload(tab.id);
+        });
+    }
+});
