b3845a704f7568cbab4aca917190f20cb5dcbe2d
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2024-09-21 21:22:10 -0600

    Refactor content script code to use IIFE modules

--- content.js
+++ content.js
@@ -1,69 +1,128 @@
-function parseText(node) {
-    let text = "";
-    if(node.nodeType === Node.TEXT_NODE) {
-        text += node.textContent;    
-    } else if(node.nodeType === Node.ELEMENT_NODE) {
-        for(let childNode of node.childNodes) {
-            let nodeHtmlTag = node.nodeName.toLowerCase();
-            let nodeDisplayStyle = window.getComputedStyle(node).getPropertyValue("display");
-            if(childNode.nodeType === Node.ELEMENT_NODE 
-                && childNode.className.includes("mw-heading") 
-                && childNode.firstChild.nodeType == Node.ELEMENT_NODE 
-                && (childNode.firstChild.getAttribute("id") === "References" || childNode.firstChild.getAttribute("id") === "Citations" )) {
-                    console.log("--reached references/citations--");
-                    break;
-            }
-            if(nodeDisplayStyle != "none" && nodeHtmlTag != "style" && nodeHtmlTag != "script" && nodeHtmlTag != "img") {
-                text += (" " + parseText(childNode));
-                // everytime parseText is called recursively, an extra space is added to the beginning of the output of that recursive call
-                // this helps with cases where multiple words are parsed without spaces(in tables so far), i.e., "LocationGreater Manchester"
+let ParserModule = (function() {
+    function isReferencesSection(node) {
+        return (node.nodeType === Node.ELEMENT_NODE 
+            && node.className.includes("mw-heading") 
+            && node.firstChild.nodeType == Node.ELEMENT_NODE 
+            && (node.firstChild.getAttribute("id") === "References" || node.firstChild.getAttribute("id") === "Citations" ))
+    }
+
+    function isExplorableNode(node) {
+        const nodeHtmlTag = node.nodeName.toLowerCase();
+        const nodeDisplayStyle = window.getComputedStyle(node).getPropertyValue("display");
+        return nodeDisplayStyle != "none" && !["style", "script", "img"].includes(nodeHtmlTag);
+    }
+
+    function printChildNodes(node) {
+        console.log(node.childNodes);
+    }
+
+   function parseText(node) {
+        let text = "";
+        if(node.nodeType === Node.TEXT_NODE) {
+            text += node.textContent;    
+        } 
+        else if(node.nodeType === Node.ELEMENT_NODE) {
+            for(let childNode of node.childNodes) {
+                if(isReferencesSection(childNode)) {
+                        console.log("--reached references/citations--");
+                        break;
+                }
+                if(isExplorableNode(node)) {
+                    text += (" " + parseText(childNode));
+                    // everytime parseText is called recursively, an extra space is added to the beginning of the output of that recursive call
+                    // this helps with cases where multiple words are parsed without spaces(in tables so far), i.e., "LocationGreater Manchester"
+                }
             }
         }
+        return text;
     }
-    return text;
-}
 
-function printChildNodes(node) {
-    console.log(node.childNodes);
-}
+    return {
+        parseText
+    }
+})();
 
-function splitArticleText(articleText, delimeter) {
-    return articleText.split(/\s+/);
-}
 
-function filterEmptyStrings(articleTextList) {
-    return articleTextList.filter(elem => elem.length > 0);
-}
+let Utils = (function() {
+    /**
+     * in: "This   is a   sentence with   irregular   spacing."
+     * out: ["This", "is", "a", "sentence", "with", "irregular", "spacing."].
+     * treats multiple consecutive spaces, tabs, or new lines as a single separator
+     * */ 
+    function splitArticleText(articleText) {
+        return articleText.split(/\s+/);
+    }
+    
+    function filterEmptyStrings(articleTextList) {
+        return articleTextList.filter(elem => elem.length > 0);
+    }
 
-async function calculateReadingTime(list) {
-    let result = await chrome.storage.local.get("wpm");
-    let wordsPerMinute = result.wpm;
-    console.log(`wpm from storage: ${wordsPerMinute}`);
-    return Math.ceil(list.length / wordsPerMinute);
-}
+    function getArticleTextList(articleText) {
+        return filterEmptyStrings(splitArticleText(articleText));
+    }
 
-function displayReadingTime(time) {
-    const title = document.getElementById("firstHeading");
-    console.log(title);
-    const readingTime = document.createElement("p");
-    readingTime.textContent = `${time} min read`;
-    readingTime.id = "readingTimeDisplay";
-    title.appendChild(readingTime);
-}
+    return {
+        getArticleTextList
+    }
+})();
 
-chrome.storage.local.get("isExtensionOn").then(result => {
-    if(result.isExtensionOn) {
-        console.log("script is running...");
-        const bodyContent = document.querySelector(".mw-content-ltr");
-        let articleText = parseText(bodyContent);
-        console.log(articleText);
-        let articleTextList =  splitArticleText(articleText);
-        console.log(articleTextList);
-        let articleTextListFiltered  = filterEmptyStrings(articleTextList);
-        console.log(articleTextListFiltered);
-        calculateReadingTime(articleTextListFiltered).then((readingTime) => {
-            console.log(`estimated reading time: ${readingTime} mins`);
-            displayReadingTime(readingTime);
+
+let TimeCalculatorModule = (function() {
+    async function getReadingTime() {
+        let result = await chrome.storage.local.get("wpm");
+        return result.wpm;
+    }
+
+    async function calculateReadingTime(list) {
+        let wordsPerMinute = await getReadingTime();
+        console.log(`wpm from storage: ${wordsPerMinute}`);
+        return Math.ceil(list.length / wordsPerMinute);
+    }
+    
+    return {
+        calculateReadingTime
+    }
+})();
+
+
+let UIModule = (function() {
+    function displayReadingTime(time) {
+        const title = document.getElementById("firstHeading");
+        console.log(title);
+        const readingTime = document.createElement("p");
+        readingTime.textContent = `${time} min read`;
+        readingTime.id = "readingTimeDisplay";
+        title.appendChild(readingTime);
+    }
+
+    return {
+        displayReadingTime
+    }
+})();
+
+
+let MainModule = (function(ParserModule, Utils, TimeCalculatorModule, UIModule) {
+    function run() {
+        chrome.storage.local.get("isExtensionOn").then(result => {
+            if(result.isExtensionOn) {
+                console.log("script is running...");
+                const bodyContent = document.querySelector(".mw-content-ltr");
+                let articleText = ParserModule.parseText(bodyContent);
+                console.log(articleText);
+                let articleTextList = Utils.getArticleTextList(articleText); 
+                console.log(articleTextList);
+                TimeCalculatorModule.calculateReadingTime(articleTextList).then((readingTime) => {
+                    console.log(`estimated reading time: ${readingTime} mins`);
+                    UIModule.displayReadingTime(readingTime);
+                });
+            }
         });
     }
-});
+
+    return {
+        run
+    }
+})(ParserModule, Utils, TimeCalculatorModule, UIModule);
+
+// entry point
+MainModule.run();
