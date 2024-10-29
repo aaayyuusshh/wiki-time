6469e9dd5844f9d72af0fe7ea226f3ec9d4995cc
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2024-10-29 17:56:44 -0600

    Mock wikipedia article dom

---
+++ tests/dom-mock.js
@@ -0,0 +1,11 @@
+const { JSDOM } = require("jsdom");
+const TEST_HTML = require("./testHtml.js")
+const { TextEncoder, TextDecoder } = require("util");
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+const dom = new JSDOM(TEST_HTML);
+global.document = dom.window.document;
+global.Node = dom.window.Node;
+global.window = dom.window;
