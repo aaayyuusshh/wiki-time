8b013c82b5dfd497801bff049461872e559e1635
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2024-11-13 23:50:42 -0700

    Test global dom mock

--- tests/mock.test.js
+++ tests/mock.test.js
@@ -1,5 +1,27 @@
-describe("DOM Setup Tests", () => {
-    it("element with content should not be null", () => {
+describe.only("DOM Mock", () => {
+    it("should have a global document", () => {
+        expect(global.document).toBeDefined();
+        expect(global.document instanceof global.window.Document).toBe(true);
+      });
+    
+      it("should have a global Node", () => {
+        expect(global.Node).toBeDefined();
+        expect(global.Node).toBe(global.window.Node);
+      });
+    
+      it("should have a global window", () => {
+        expect(global.window).toBeDefined();
+        expect(typeof global.window).toBe("object");
+      });
+    
+      it("should define TextEncoder and TextDecoder", () => {
+        expect(global.TextEncoder).toBeDefined();
+        expect(global.TextDecoder).toBeDefined();
+        expect(new global.TextEncoder()).toBeInstanceOf(TextEncoder);
+        expect(new global.TextDecoder()).toBeInstanceOf(TextDecoder);
+      });
+
+      it("should have .mw-content-ltr root element", () => {
         const contentElement = global.document.querySelector(".mw-content-ltr");
         expect(contentElement).not.toBeNull();
     });
